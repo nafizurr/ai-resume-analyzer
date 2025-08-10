2381ae1e49c66d2f83660fad019f240c653452e6
nafizurr <nafizurr@buffalo.edu>
2025-08-10 00:35:09 -0400

    Finished Most of the FrontEnd and Fixed Up the spacing issue

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,36 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import * as domain from "node:domain";
+import Navbar from "~/components/Navbar";
+import {resumes} from "../../constants";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumind" },
+    { name: "description", content: "Smarter Feedback To Land Your Dream Job" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+    return <main className = "bg-[url('/images/bg-main.svg')] bg-cover">
+        <Navbar />
+        <section className="main-section">
+            <div className ="page-heading">
+                <h1>Track Your Applications & Resume Ratings</h1>
+                <h2>Review Your Submissions and Receive AI Powered Feedback</h2>
+            </div>
+
+            {resumes.length > 0 && (
+                <div className ="resumes-section">
+                    {resumes.map((resume) => (
+                        <ResumeCard key ={resume.id} resume = {resume} />
+                    ))}
+                </div>
+            )}
+
+    </section>
+
+    </main>
 }
+
+
